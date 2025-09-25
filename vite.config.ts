b55bd53e64bdfe68cb51579e995825d35afffce5
You Song Hou <ysongweb3@gmail.com>
2025-09-24 20:26:36 -0400

    feat: integrate SecretVaults SDK

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
